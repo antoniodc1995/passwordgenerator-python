1c786e00afb1b7c4c32d7788a3438512dc0fe234
antoniodc1995 <a.dececio1995@gmail.com>
2024-07-29 23:45:05 +0200

    modified:   passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,5 +1,17 @@
 import random
 password = "ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+{}:<>"
-length_password = int(input("Enter the length: of the password: "))
+
+max_length = len(password)
+
+while True:
+    try:
+        length_password = int(input(f"Enter the length of the password (1-{max_length}): "))
+        if 1 <= length_password <= max_length:
+            break
+        else:
+            print(f"Please enter a number between 1 and {max_length}.")
+    except ValueError:
+        print("Please enter a valid integer.")
+
 a= "".join(random.sample(password, length_password))
 print(f"Your password is: {a}")
